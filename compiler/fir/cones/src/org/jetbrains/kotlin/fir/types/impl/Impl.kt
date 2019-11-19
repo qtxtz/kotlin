5cec4c55556d4d9da44d864d5b13773ce1dc8342
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-11-19 18:29:04 +0300

    FIR: Add cache for expanded types

--- compiler/fir/cones/src/org/jetbrains/kotlin/fir/types/impl/Impl.kt
+++ compiler/fir/cones/src/org/jetbrains/kotlin/fir/types/impl/Impl.kt
@@ -16,6 +16,9 @@ open class ConeClassLikeTypeImpl(
     isNullable: Boolean
 ) : ConeClassLikeType() {
     override val nullability: ConeNullability = ConeNullability.create(isNullable)
+
+    var expandedType: Pair<*, ConeClassLikeType>? = null
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
