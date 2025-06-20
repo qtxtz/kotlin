76100dbace6ac0a76c87c7ea19110f5ffe15483d
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-06-20 22:26:18 +0200

    [IR] Drop old param APIs in IrMemberAccessExpression
    #KT-76934 Fixed

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/print/DeepCopyIrTreeWithSymbolsPrinter.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/print/DeepCopyIrTreeWithSymbolsPrinter.kt
@@ -217,10 +217,6 @@ internal class DeepCopyIrTreeWithSymbolsPrinter(
                     }
                 }
             }
-            if (element.isSubclassOf(IrTree.memberAccessExpression) && !element.isSubclassOf(IrTree.localDelegatedPropertyReference)) {
-                println("copyRemappedTypeArgumentsFrom(", element.visitorParameterName, ")")
-                println("transformValueArguments(", element.visitorParameterName, ")")
-            }
             if (element.isSubclassOf(IrTree.function)) {
                 println("parameters = ${element.visitorParameterName}.parameters.memoryOptimizedMap { it.transform() }")
             }
