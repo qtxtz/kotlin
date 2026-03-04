0fb3aa1d55cc17e9ee732458f80b4ff01c0c31fd
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2026-03-04 19:34:28 +0100

    [AA] Add test for code fragments with an unstable dangling file context
    - The test covers a case where we have
      `physical --> physical --> non-physical` dangling files.
    - The whole chain is expected to be unstable,
      i.e. `unstable --> unstable --> unstable`, because of the non-physical
      dangling file at the base.
    
    ^KT-84701

--- analysis/analysis-api-fir/tests/org/jetbrains/kotlin/analysis/api/fir/test/projectStructure/AnalysisApiDanglingFilesTest.kt
+++ analysis/analysis-api-fir/tests/org/jetbrains/kotlin/analysis/api/fir/test/projectStructure/AnalysisApiDanglingFilesTest.kt
@@ -6,7 +6,9 @@
 package org.jetbrains.kotlin.analysis.api.fir.test.projectStructure
 
 import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.platform.modification.publishGlobalModuleStateModificationEvent
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileModule
+import org.jetbrains.kotlin.analysis.api.projectStructure.isStable
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirSourceTestConfigurator
 import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiExecutionTest
 import org.jetbrains.kotlin.psi.KtCallExpression
@@ -17,6 +19,7 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.assertions
+import org.jetbrains.kotlin.test.testFramework.runWriteAction
 import org.junit.jupiter.api.Test
 import kotlin.reflect.full.isSubclassOf
 
@@ -75,6 +78,99 @@ class AnalysisApiDanglingFilesTest : AbstractAnalysisApiExecutionTest("testData/
 
             // Elements from the original fragment should be analyzable in the context of copy
             assertions.assertTrue(codeFragmentExpression.canBeAnalysed())
+        }
+    }
+
+    /**
+     * The test covers a situation where a physical code fragment depends on a non-physical dangling file. The dangling file of the code
+     * fragment is expected to be unstable since it depends on an unstable context module.
+     *
+     * The test ensures that we can correctly create and work with such `unstable --> unstable` LL FIR sessions (see KT-84701).
+     */
+    @Test
+    fun codeFragmentsWithUnstableContext(mainFile: KtFile, testServices: TestServices) {
+        val assertions = testServices.assertions
+
+        // Disabled event system --> unstable dangling file.
+        val ktPsiFactory = KtPsiFactory.contextual(mainFile, markGenerated = true, eventSystemEnabled = false)
+        val danglingFile = ktPsiFactory.createFile("fake.kt", mainFile.text)
+
+        analyze(danglingFile) {
+            val useSiteModule = useSiteModule as? KaDanglingFileModule
+                ?: error("Expected the use-site module to be a dangling file module")
+
+            assertions.assertFalse(useSiteModule.isStable) { "Expected the dangling file to be unstable." }
+        }
+
+        val simpleClass = danglingFile.declarations.single() as KtClass
+        val method = simpleClass.declarations.first() as KtNamedFunction
+        assertions.assertEquals("method", method.name)
+
+        val context = simpleClass.declarations.last() as KtNamedFunction
+        assertions.assertEquals("context", context.name)
+
+        val codeFragment = KtPsiFactory(mainFile.project).createExpressionCodeFragment("method()", context)
+        val codeFragmentExpression = codeFragment.getContentElement() as KtCallExpression
+
+        analyze(codeFragmentExpression) {
+            val useSiteModule = useSiteModule as? KaDanglingFileModule
+                ?: error("Expected the use-site module to be a dangling file module")
+
+            // Even though the code fragment is most definitely physical, it should still be unstable because its context module is
+            // unstable.
+            assertions.assertFalse(useSiteModule.isStable) { "Expected the code fragment to be unstable." }
+
+            val callableSymbol = codeFragmentExpression.resolveSymbol()
+
+            // Check whether the code fragment is configured correctly.
+            assertions.assertEquals(method, callableSymbol?.psi)
+
+            // Check whether elements from the dangling file are analyzable.
+            assertions.assertTrue(simpleClass.canBeAnalysed())
+        }
+
+        // Last, we also check a physical code fragment depending on the above code fragment. To illustrate, we have the following
+        // constellation:
+        //
+        // physical --> physical --> non-physical
+        //
+        // To ensure that `isStable` is correctly (recursively) calculated, we should check this case explicitly.
+        val nestedCodeFragment = KtPsiFactory(mainFile.project).createExpressionCodeFragment("method()", codeFragmentExpression)
+        val nestedCodeFragmentExpression = nestedCodeFragment.getContentElement() as KtCallExpression
+
+        analyze(nestedCodeFragmentExpression) {
+            val useSiteModule = useSiteModule as? KaDanglingFileModule
+                ?: error("Expected the use-site module to be a dangling file module")
+
+            assertions.assertFalse(useSiteModule.isStable) { "Expected the nested code fragment to be unstable." }
+
+            val callableSymbol = nestedCodeFragmentExpression.resolveSymbol()
+
+            // Check whether the code fragment is configured correctly.
+            assertions.assertEquals(method, callableSymbol?.psi)
+
+            // Check whether elements from the dangling file are analyzable.
+            assertions.assertTrue(simpleClass.canBeAnalysed())
+
+            // Elements from the original fragment should be analyzable in the context of copy.
+            assertions.assertTrue(codeFragmentExpression.canBeAnalysed())
+        }
+
+        analyze(codeFragmentExpression) {
+            // Elements from the nested code fragment are not expected to be analyzable in the context of the original code fragment,
+            // since each code fragment has its own module.
+            assertions.assertFalse(nestedCodeFragmentExpression.canBeAnalysed())
+        }
+
+        // To test the situation where nested creation leads to a recursive update exception (see KT-84701), we have to reset session caches
+        // and request the outermost session right away. (The order of analysis matters for session creation: If the inner sessions are
+        // created one by one on `analyze`, when an outer session requires its context module session, it will already exist in the cache.)
+        runWriteAction {
+            mainFile.project.publishGlobalModuleStateModificationEvent()
+        }
+
+        analyze(nestedCodeFragmentExpression) {
+            assertions.assertTrue(useSiteModule::class.isSubclassOf(KaDanglingFileModule::class))
         }
     }
 }
