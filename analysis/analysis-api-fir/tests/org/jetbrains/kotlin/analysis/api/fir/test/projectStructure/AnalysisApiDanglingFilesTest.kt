baf7d209c9328b938a65de4f71bef4a8c02e2a80
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2026-03-04 19:32:09 +0100

    [AA] Refactoring: Move `codeFragmentCopy` test to its own test class
    - In preparation for additional explicit dangling file tests.
    
    ^KT-84701

---
+++ analysis/analysis-api-fir/tests/org/jetbrains/kotlin/analysis/api/fir/test/projectStructure/AnalysisApiDanglingFilesTest.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.fir.test.projectStructure
+
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileModule
+import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirSourceTestConfigurator
+import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiExecutionTest
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtExpressionCodeFragment
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.assertions
+import org.junit.jupiter.api.Test
+import kotlin.reflect.full.isSubclassOf
+
+/**
+ * Custom Analysis API tests covering dangling files, including code fragments.
+ */
+class AnalysisApiDanglingFilesTest : AbstractAnalysisApiExecutionTest("testData/projectStructure/danglingFiles") {
+    override val configurator = AnalysisApiFirSourceTestConfigurator(analyseInDependentSession = false)
+
+    /**
+     * See KT-79109.
+     */
+    @Test
+    fun codeFragmentCopy(mainFile: KtFile, testServices: TestServices) {
+        val assertions = testServices.assertions
+
+        val simpleClass = mainFile.declarations.single() as KtClass
+        val method = simpleClass.declarations.first() as KtNamedFunction
+        assertions.assertEquals("method", method.name)
+
+        val context = simpleClass.declarations.last() as KtNamedFunction
+        assertions.assertEquals("context", context.name)
+
+        val codeFragment = KtPsiFactory(mainFile.project).createExpressionCodeFragment("method()", context)
+        val codeFragmentExpression = codeFragment.getContentElement() as KtCallExpression
+
+        val codeFragmentCopy = codeFragment.copy() as KtExpressionCodeFragment
+        val codeFragmentCopyExpression = codeFragmentCopy.getContentElement() as KtCallExpression
+
+        analyze(codeFragmentExpression) {
+            assertions.assertTrue(useSiteModule::class.isSubclassOf(KaDanglingFileModule::class))
+
+            val callableSymbol = codeFragmentExpression.resolveSymbol()
+
+            // Check whether the code fragment is configured correctly
+            assertions.assertEquals(method, callableSymbol?.psi)
+
+            // Check whether elements from the original file are analyzable
+            assertions.assertTrue(simpleClass.canBeAnalysed())
+
+            // Elements from the fragment copy are not expected to be analyzable in the context of original
+            // since each code fragment has its own module
+            assertions.assertFalse(codeFragmentCopyExpression.canBeAnalysed())
+        }
+
+        analyze(codeFragmentCopyExpression) {
+            assertions.assertTrue(useSiteModule::class.isSubclassOf(KaDanglingFileModule::class))
+
+            val callableSymbol = codeFragmentCopyExpression.resolveSymbol()
+
+            // Check whether the code fragment copy is configured correctly
+            assertions.assertEquals(method, callableSymbol?.psi)
+
+            // Check whether elements from the original file are analyzable
+            assertions.assertTrue(simpleClass.canBeAnalysed())
+
+            // Elements from the original fragment should be analyzable in the context of copy
+            assertions.assertTrue(codeFragmentExpression.canBeAnalysed())
+        }
+    }
+}
