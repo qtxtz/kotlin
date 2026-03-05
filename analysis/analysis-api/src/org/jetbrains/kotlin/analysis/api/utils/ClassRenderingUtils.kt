1b552678da55ac097aec19c8e5eff58f0e482ea4
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2026-03-05 15:57:50 +0900

    [Analysis API] Make 'renderAsDataClassToString()' internal
    ^KT-84932

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/utils/ClassRenderingUtils.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/utils/ClassRenderingUtils.kt
@@ -3,14 +3,14 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.analysis.utils.reflection
+package org.jetbrains.kotlin.analysis.api.utils
 
+import org.jetbrains.kotlin.analysis.utils.printer.prettyPrint
 import java.lang.reflect.InvocationTargetException
 import kotlin.reflect.full.declaredMemberProperties
-import org.jetbrains.kotlin.analysis.utils.printer.prettyPrint
 import kotlin.reflect.jvm.isAccessible
 
-public fun Any.renderAsDataClassToString(): String = prettyPrint {
+internal fun Any.renderAsDataClassToString(): String = prettyPrint {
     append(this@renderAsDataClassToString::class.qualifiedName)
     append("(")
     printCollection(this@renderAsDataClassToString::class.declaredMemberProperties) { property ->
@@ -20,7 +20,7 @@ public fun Any.renderAsDataClassToString(): String = prettyPrint {
         try {
             getter.isAccessible = true
             append(getter.call(this@renderAsDataClassToString).toString())
-        } catch (e: InvocationTargetException) {
+        } catch (_: InvocationTargetException) {
             append("ERROR_RENDERING_FIELD")
         } catch (_: Exception) {
             println("")
