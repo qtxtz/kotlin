a66f592401cda5fff20e8da28633086405206d87
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-03-20 23:28:14 +0100

    [psi] move OperatorNamesToSymbolsTest to psi-impl module

--- compiler/psi/psi-impl/tests/org/jetbrains/kotlin/psi/OperatorNamesToSymbolsTest.kt
+++ compiler/psi/psi-impl/tests/org/jetbrains/kotlin/psi/OperatorNamesToSymbolsTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.lexer.KtSingleValueToken
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.types.expressions.OperatorConventions
 import org.jetbrains.kotlin.util.OperatorNameConventions
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
