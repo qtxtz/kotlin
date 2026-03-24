qtxtz/kotlin

commit 80bbc4b61da455daa8a65b30afb127da5870b2a9

Reflection: support descriptor-less Kotlin constructors
 #KT-80710

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKParameter.kt
@@ -5,10 +5,6 @@
 
 package kotlin.reflect.jvm.internal
 
-import java.lang.reflect.Constructor
-import java.lang.reflect.Member
-import java.lang.reflect.Method
-import java.lang.reflect.Modifier
 import kotlin.LazyThreadSafetyMode.PUBLICATION
 import kotlin.metadata.KmValueParameter
 import kotlin.metadata.declaresDefaultValue
@@ -23,13 +19,13 @@ internal class KotlinKParameter(
     typeParameterTable: TypeParameterTable,
 ) : ReflectKParameter() {
     override val name: String? =
-        kmParameter.name.takeUnless { kind == KParameter.Kind.EXTENSION_RECEIVER }
+        kmParameter.name.takeUnless { it.startsWith("<") }
 
     override val type: KType by lazy(PUBLICATION) {
         kmParameter.type.toKType(callable.container.jClass.classLoader, typeParameterTable) {
-            require(callable.container is KPackageImpl) {
+            require(callable.container is KPackageImpl || callable.isConstructor) {
                 // For class callables, we'll also need to tweak instance receiver parameter type (see `DescriptorKParameter`).
-                "Only top-level callables are supported for now: $callable"
+                "Only constructors and top-level callables are supported for now: $callable"
             }
             callable.caller.parameterTypes[index]
         }
@@ -37,9 +33,9 @@ internal class KotlinKParameter(
 
     override val isOptional: Boolean
         get() {
-            require(callable is KotlinKProperty<*> || callable.container is KPackageImpl) {
+            require(callable is KotlinKProperty<*> || callable.container is KPackageImpl || callable.isConstructor) {
                 // For class functions, we'll also need to check the flag for parameters from inherited functions.
-                "Only top-level callables are supported for now: $callable"
+                "Only constructors and top-level callables are supported for now: $callable"
             }
             return kmParameter.declaresDefaultValue
         }
@@ -49,35 +45,4 @@ internal class KotlinKParameter(
 
     override val isVararg: Boolean
         get() = kmParameter.varargElementType != null
-
-    override val annotations: List<Annotation> by lazy(PUBLICATION) {
-        val java = javaParameter
-        when (val callable = java?.callable) {
-            is Method -> callable.parameterAnnotations[java.index].toList()
-            is Constructor<*> -> callable.parameterAnnotations[java.index].toList()
-            else -> emptyList()
-        }.unwrapKotlinRepeatableAnnotations()
-    }
 }
-
-/**
- * Represents a parameter in Java reflection. Unfortunately, there's no good representation of parameters in Java reflection, and we can't
- * use [java.lang.reflect.Parameter] because it's only available with javac's `-parameters` (or Kotlin's `-java-parameters`) option,
- * and requires API level >= 26 on Android.
- *
- * @param callable method or constructor, which this parameter belongs to.
- * @param index index of this parameter in [callable]'s parameter list. Note that dispatch receiver parameter is not a part of parameters
- *   in Java reflection, so the index goes only through context, extension receiver, and value parameters.
- */
-private class JavaParameter(val callable: Member, val index: Int)
-
-private val KotlinKParameter.javaParameter: JavaParameter?
-    get() {
-        return when (val callable = callable.caller.member) {
-            is Method -> {
-                require(Modifier.isStatic(callable.modifiers)) { "Only static methods are supported for now: $callable" }
-                JavaParameter(callable, index)
-            }
-            else -> error("Only methods are supported for now: $callable")
-        }
-    }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ReflectKParameter.kt
@@ -5,12 +5,29 @@
 
 package kotlin.reflect.jvm.internal
 
+import java.lang.reflect.Constructor
+import java.lang.reflect.Member
+import java.lang.reflect.Method
+import java.lang.reflect.Modifier
+import kotlin.LazyThreadSafetyMode.PUBLICATION
+import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
+import kotlin.reflect.KType
+import kotlin.reflect.full.createDefaultType
 
 internal abstract class ReflectKParameter : KParameter {
     abstract val callable: ReflectKCallable<*>
 
     abstract val declaresDefaultValue: Boolean
+
+    override val annotations: List<Annotation> by lazy(PUBLICATION) {
+        val java = javaParameter
+        when (val callable = java?.callable) {
+            is Method -> callable.parameterAnnotations[java.index].toList()
+            is Constructor<*> -> callable.parameterAnnotations[java.index].toList()
+            else -> emptyList()
+        }.unwrapKotlinRepeatableAnnotations()
+    }
 
     final override fun equals(other: Any?): Boolean =
         other is ReflectKParameter && callable == other.callable && index == other.index
@@ -21,3 +38,49 @@ internal abstract class ReflectKParameter : KParameter {
     final override fun toString(): String =
         ReflectionObjectRenderer.renderParameter(this)
 }
+
+internal class InstanceParameter(override val callable: ReflectKCallable<*>, klass: KClass<*>) : ReflectKParameter() {
+    override val index: Int get() = 0
+    override val type: KType = klass.createDefaultType()
+    override val name: String? get() = null
+    override val kind: KParameter.Kind get() = KParameter.Kind.INSTANCE
+    override val isOptional: Boolean get() = false
+    override val isVararg: Boolean get() = false
+    override val annotations: List<Annotation> get() = emptyList()
+    override val declaresDefaultValue: Boolean get() = false
+}
+
+/**
+ * Represents a parameter in Java reflection. Unfortunately, there's no good representation of parameters in Java reflection, and we can't
+ * use [java.lang.reflect.Parameter] because it's only available with javac's `-parameters` (or Kotlin's `-java-parameters`) option,
+ * and requires API level >= 26 on Android.
+ *
+ * @param callable method or constructor, which this parameter belongs to.
+ * @param index index of this parameter in [callable]'s parameter list. Note that dispatch receiver parameter is not a part of parameters
+ *   in Java reflection, so the index goes only through context, extension receiver, and value parameters.
+ */
+internal class JavaParameter(val callable: Member, val index: Int)
+
+internal val ReflectKParameter.javaParameter: JavaParameter?
+    get() = when (val callable = callable.caller.member) {
+        is Method -> {
+            require(Modifier.isStatic(callable.modifiers)) { "Only static methods are supported for now: $callable" }
+            JavaParameter(callable, index)
+        }
+        is Constructor<*> -> {
+            val shift = when {
+                // Inner class constructors before JDK 9 did not have the outer class parameter in `parameterAnnotations`, see
+                // https://bugs.java.com/bugdatabase/view_bug?bug_id=8074977.
+                callable.declaringClass.kotlin.isInner && isJdk8() -> -1
+                // Enum constructors before JDK 17 did not have additional name/ordinal parameters in case there was at least one annotation
+                // on any constructor parameter. (Probably some fixed bug in the JDK as well.)
+                callable.declaringClass.isEnum -> callable.parameterAnnotations.size - callable.parameterTypes.size + 2
+                else -> 0
+            }
+            JavaParameter(callable, index + shift)
+        }
+        else -> throw KotlinReflectionInternalError("Unsupported parameter owner: $callable")
+    }
+
+private fun isJdk8(): Boolean =
+    System.getProperty("java.version")?.startsWith("1.") == true

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLBlackBoxTestGenerated.java
@@ -54511,6 +54511,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       }
 
       @Test
+      @TestMetadata("constructorParameters.kt")
+      public void testConstructorParameters() {
+        runTest("compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt");
+      }
+
+      @Test
       @TestMetadata("findAnnotation.kt")
       public void testFindAnnotation() {
         runTest("compiler/testData/codegen/box/reflection/annotations/findAnnotation.kt");
@@ -55114,6 +55120,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
         }
 
         @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt");
+        }
+
+        @Test
         @TestMetadata("secondaryConstructorVisibilies.kt")
         public void testSecondaryConstructorVisibilies() {
           runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/secondaryConstructorVisibilies.kt");
@@ -55661,6 +55673,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
           KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/reflection/callBy/inlineClasses"), Pattern.compile("^(.+)\\.kt$"), null, true);
         }
 
+        @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt");
+        }
+
         @Nested
         @TestMetadata("compiler/testData/codegen/box/reflection/callBy/inlineClasses/nonNullObject")
         @TestDataPath("$PROJECT_ROOT")
@@ -56179,6 +56197,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       }
 
       @Test
+      @TestMetadata("innerClassOfGenericClass.kt")
+      public void testInnerClassOfGenericClass() {
+        runTest("compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt");
+      }
+
+      @Test
       @TestMetadata("primaryConstructor.kt")
       public void testPrimaryConstructor() {
         runTest("compiler/testData/codegen/box/reflection/constructors/primaryConstructor.kt");
@@ -56592,12 +56616,6 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       @TestMetadata("constructor.kt")
       public void testConstructor() {
         runTest("compiler/testData/codegen/box/reflection/mapping/constructor.kt");
-      }
-
-      @Test
-      @TestMetadata("constructorWithInlineClassParameters.kt")
-      public void testConstructorWithInlineClassParameters() {
-        runTest("compiler/testData/codegen/box/reflection/mapping/constructorWithInlineClassParameters.kt");
       }
 
       @Test

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
@@ -54511,6 +54511,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       }
 
       @Test
+      @TestMetadata("constructorParameters.kt")
+      public void testConstructorParameters() {
+        runTest("compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt");
+      }
+
+      @Test
       @TestMetadata("findAnnotation.kt")
       public void testFindAnnotation() {
         runTest("compiler/testData/codegen/box/reflection/annotations/findAnnotation.kt");
@@ -55114,6 +55120,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
         }
 
         @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt");
+        }
+
+        @Test
         @TestMetadata("secondaryConstructorVisibilies.kt")
         public void testSecondaryConstructorVisibilies() {
           runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/secondaryConstructorVisibilies.kt");
@@ -55661,6 +55673,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
           KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/reflection/callBy/inlineClasses"), Pattern.compile("^(.+)\\.kt$"), null, true);
         }
 
+        @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt");
+        }
+
         @Nested
         @TestMetadata("compiler/testData/codegen/box/reflection/callBy/inlineClasses/nonNullObject")
         @TestDataPath("$PROJECT_ROOT")
@@ -56179,6 +56197,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       }
 
       @Test
+      @TestMetadata("innerClassOfGenericClass.kt")
+      public void testInnerClassOfGenericClass() {
+        runTest("compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt");
+      }
+
+      @Test
       @TestMetadata("primaryConstructor.kt")
       public void testPrimaryConstructor() {
         runTest("compiler/testData/codegen/box/reflection/constructors/primaryConstructor.kt");
@@ -56592,12 +56616,6 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       @TestMetadata("constructor.kt")
       public void testConstructor() {
         runTest("compiler/testData/codegen/box/reflection/mapping/constructor.kt");
-      }
-
-      @Test
-      @TestMetadata("constructorWithInlineClassParameters.kt")
-      public void testConstructorWithInlineClassParameters() {
-        runTest("compiler/testData/codegen/box/reflection/mapping/constructorWithInlineClassParameters.kt");
       }
 
       @Test

compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt
@@ -1,0 +1,60 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FILE: test/J.java
+package test;
+
+public class J {
+    public J(@Anno("J") String s) {}
+
+    public class Inner {
+        public Inner(@Anno("Inner") int x) {}
+    }
+}
+
+// FILE: test/JEnum.java
+package test;
+
+public enum JEnum {
+    ;
+    JEnum(@Anno("JEnum") double d) {}
+}
+
+// FILE: test/box.kt
+package test
+
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
+
+annotation class Anno(val value: String)
+
+class K(@Anno("K") f: Float) {
+    inner class Inner(@Anno("Inner") j: Long)
+}
+
+enum class KEnum(@Anno("KEnum") z: Boolean)
+
+sealed class Sealed(@Anno("Sealed") s: Short)
+
+class Default(@Anno("Default") n: Number? = null)
+
+sealed class SealedWithDefault(@Anno("SealedWithDefault") a: Any? = null)
+
+private val KClass<*>.ctorParamAnnotations: String
+    get() = constructors.single().parameters.joinToString(", ") { p ->
+        p.annotations.map { (it as Anno).value }.toString()
+    }
+
+fun box(): String {
+    assertEquals("[J]", J::class.ctorParamAnnotations)
+    assertEquals("[], [Inner]", J.Inner::class.ctorParamAnnotations)
+    assertEquals("[JEnum]", JEnum::class.ctorParamAnnotations)
+
+    assertEquals("[K]", K::class.ctorParamAnnotations)
+    assertEquals("[], [Inner]", K.Inner::class.ctorParamAnnotations)
+    assertEquals("[KEnum]", KEnum::class.ctorParamAnnotations)
+    assertEquals("[Sealed]", Sealed::class.ctorParamAnnotations)
+    assertEquals("[Default]", Default::class.ctorParamAnnotations)
+    assertEquals("[SealedWithDefault]", SealedWithDefault::class.ctorParamAnnotations)
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt
@@ -1,0 +1,18 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+
+import kotlin.reflect.jvm.isAccessible
+
+class C(val x: Result<Int>)
+
+fun box(): String {
+    val resultCtor = Result::class.constructors.single()
+    val r = resultCtor.apply { isAccessible = true }.call(42)
+    if (r != Result.success(42)) return "Fail 1: $r"
+
+    val ctorWithResult = ::C
+    val s = ctorWithResult.call(r).x
+    if (s != Result.success(42)) return "Fail 2: $s"
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt
@@ -1,0 +1,18 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+
+import kotlin.reflect.jvm.isAccessible
+
+class C(val x: Result<Int>)
+
+fun box(): String {
+    val resultCtor = Result::class.constructors.single()
+    val r = resultCtor.apply { isAccessible = true }.callBy(mapOf(resultCtor.parameters.single() to 42))
+    if (r != Result.success(42)) return "Fail 1: $r"
+
+    val ctorWithResult = ::C
+    val s = ctorWithResult.callBy(mapOf(ctorWithResult.parameters.single() to r)).x
+    if (s != Result.success(42)) return "Fail 2: $s"
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/constructors/classesWithoutConstructors.kt
@@ -2,7 +2,7 @@
 // WITH_REFLECT
 
 import kotlin.coroutines.SuspendFunction0
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 interface Interface
 object Obj
@@ -12,12 +12,12 @@ class C {
 }
 
 fun box(): String {
-    assertTrue(Interface::class.constructors.isEmpty())
-    assertTrue(Obj::class.constructors.isEmpty())
-    assertTrue(C.Companion::class.constructors.isEmpty())
-    assertTrue(object {}::class.constructors.isEmpty())
-    assertTrue(Function0::class.constructors.isEmpty())
-    assertTrue(SuspendFunction0::class.constructors.isEmpty())
+    assertEquals(emptyList(), Interface::class.constructors)
+    assertEquals(emptyList(), Obj::class.constructors)
+    assertEquals(emptyList(), C.Companion::class.constructors)
+    assertEquals(emptyList(), object {}::class.constructors)
+    assertEquals(emptyList(), Function0::class.constructors)
+    assertEquals(emptyList(), SuspendFunction0::class.constructors)
 
     return "OK"
 }

commit ecdd679eb41f73351d321f9c80f7a9053831ee73

Reflection: support annotations on annotation parameters
 #RCA: caused by 80bbc4b61d (part of KT-80710) where annotation
constructor parameters were overlooked. Note that annotations on
constructor parameters were never loaded in the compiler because of a
behavior change described in KT-22463.

 #KT-85091 Fixed

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKParameter.kt
@@ -13,7 +13,7 @@ import kotlin.reflect.KType
 
 internal class KotlinKParameter(
     override val callable: KotlinKCallable<*>,
-    private val kmParameter: KmValueParameter,
+    internal val kmParameter: KmValueParameter,
     override val index: Int,
     override val kind: KParameter.Kind,
     typeParameterTable: TypeParameterTable,

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKProperty.kt
@@ -84,8 +84,8 @@ internal abstract class KotlinKProperty<out V>(
 
     override val annotations: List<Annotation>
         get() {
-            if (isLocalDelegated) {
-                // Annotations on local delegated properties are present only in the metadata.
+            if (isLocalDelegated || container.jClass.isAnnotation) {
+                // Annotations on local delegated properties and annotation constructor properties are present only in the metadata.
                 @OptIn(ExperimentalAnnotationsInMetadata::class)
                 return kmProperty.annotations.map { it.toAnnotation(container.jClass.classLoader) }
             }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ReflectKParameter.kt
@@ -10,6 +10,7 @@ import java.lang.reflect.Member
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 import kotlin.LazyThreadSafetyMode.PUBLICATION
+import kotlin.metadata.ExperimentalAnnotationsInMetadata
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.KType
@@ -21,6 +22,8 @@ internal abstract class ReflectKParameter : KParameter {
     abstract val declaresDefaultValue: Boolean
 
     override val annotations: List<Annotation> by lazy(PUBLICATION) {
+        if (callable.isAnnotationConstructor) return@lazy loadAnnotationsOnAnnotationParameter()
+
         val java = javaParameter
         when (val callable = java?.callable) {
             is Method -> callable.parameterAnnotations[java.index].toList()
@@ -48,6 +51,15 @@ internal class InstanceParameter(override val callable: ReflectKCallable<*>, kla
     override val isVararg: Boolean get() = false
     override val annotations: List<Annotation> get() = emptyList()
     override val declaresDefaultValue: Boolean get() = false
+}
+
+private fun ReflectKParameter.loadAnnotationsOnAnnotationParameter(): List<Annotation> {
+    // In Java, there's no notion of annotation constructors.
+    if (this !is KotlinKParameter) return emptyList()
+
+    // In Kotlin, parameters of annotation constructors have no annotations in JVM bytecode, so we load them from metadata.
+    @OptIn(ExperimentalAnnotationsInMetadata::class)
+    return kmParameter.annotations.map { it.toAnnotation(callable.container.jClass.classLoader) }
 }
 
 /**

compiler/testData/codegen/boxJvm/reflection/annotations/annotationParameter.kt
@@ -1,0 +1,35 @@
+// IGNORE_BACKEND_K1: JVM_IR
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// LANGUAGE: +PropertyParamAnnotationDefaultTargetMode
+package test
+
+import kotlin.reflect.KProperty1
+import kotlin.test.assertEquals
+
+annotation class Anno(val value: Int)
+
+annotation class A(val x: String, @Anno(1) val y: String)
+
+fun box(): String {
+    // We still use K1 for member properties, and K1 lacks the support of annotations in metadata.
+    // As soon as we switch to Km-based properties, annotation will appear here.
+    assertEquals(
+        "[[], []]",
+        A::class.members.filter { it is KProperty1<*, *> }.map { it.annotations }.toString(),
+    )
+
+    if (Class.forName("kotlin.reflect.jvm.internal.SystemPropertiesKt").getMethod("getUseK1Implementation").invoke(null) == true) {
+        // K1 did not support annotations on annotation parameters.
+        assertEquals(
+            "[[], []]",
+            A::class.constructors.single().parameters.map { it.annotations }.toString(),
+        )
+    } else {
+        assertEquals(
+            "[[], [@test.Anno(value=1)]]",
+            A::class.constructors.single().parameters.map { it.annotations }.toString(),
+        )
+    }
+    return "OK"
+}

compiler/testData/codegen/boxJvm/reflection/annotations/annotationParameter_lv23.kt
@@ -1,0 +1,34 @@
+// IGNORE_BACKEND_K1: JVM_IR
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// LANGUAGE: -PropertyParamAnnotationDefaultTargetMode
+package test
+
+import kotlin.reflect.KProperty1
+import kotlin.test.assertEquals
+
+annotation class Anno(val value: Int)
+
+annotation class A(val x: String, @Anno(1) val y: String)
+
+fun box(): String {
+    // No annotation here because before 2.4, the annotation was applied only to the parameter.
+    assertEquals(
+        "[[], []]",
+        A::class.members.filter { it is KProperty1<*, *> }.map { it.annotations }.toString(),
+    )
+
+    if (Class.forName("kotlin.reflect.jvm.internal.SystemPropertiesKt").getMethod("getUseK1Implementation").invoke(null) == true) {
+        // K1 did not support annotations on annotation parameters.
+        assertEquals(
+            "[[], []]",
+            A::class.constructors.single().parameters.map { it.annotations }.toString(),
+        )
+    } else {
+        assertEquals(
+            "[[], [@test.Anno(value=1)]]",
+            A::class.constructors.single().parameters.map { it.annotations }.toString(),
+        )
+    }
+    return "OK"
+}