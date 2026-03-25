bd388896f9e181a0fc56049968626492cc072279
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-03-25 12:35:15 +0100

    [Tests] Add opt-in tests

---
+++ compiler/testData/diagnostics/testsWithStdLib/experimental/staticImportOfObjectMember.kt
@@ -0,0 +1,16 @@
+// RUN_PIPELINE_TILL: FRONTEND
+import O.objMember
+
+@RequiresOptIn
+annotation class A
+
+@A
+object O {
+    fun objMember() {}
+}
+
+fun test() {
+    <!OPT_IN_USAGE_ERROR!>objMember<!>()
+}
+
+/* GENERATED_FIR_TAGS: annotationDeclaration, functionDeclaration, objectDeclaration */
