ceaf78bfc25d6194ec193573780a7e7276554de3
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2026-04-02 15:57:31 +0200

    [Wasm] require is exposed by the compiler
    ^KT-82073 fixed

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/mpp-wasm-js-browser-nodejs/src/wasmJsTest/kotlin/FooTest.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/mpp-wasm-js-browser-nodejs/src/wasmJsTest/kotlin/FooTest.kt
@@ -4,7 +4,6 @@ import kotlin.test.Ignore
 
 class TestClient {
     @Test
-    @Ignore // KT-82073: requires exposing require into import-objects file
     fun testGreet() {
         assertFalse("No require found", ::checkRequire)
     }
