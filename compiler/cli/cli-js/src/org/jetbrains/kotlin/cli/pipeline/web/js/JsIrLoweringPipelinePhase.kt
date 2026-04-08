4030e7ff678a1a26657f6eb384b2db5909e24940
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-04-08 18:46:46 +0200

    [JS] Inline the only usage of the `compileIr` function

--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/pipeline/web/js/JsIrLoweringPipelinePhase.kt
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/pipeline/web/js/JsIrLoweringPipelinePhase.kt
@@ -6,43 +6,103 @@
 package org.jetbrains.kotlin.cli.pipeline.web.js
 
 import org.jetbrains.kotlin.backend.common.CompilationException
+import org.jetbrains.kotlin.backend.common.linkage.issues.checkNoUnboundSymbols
 import org.jetbrains.kotlin.cli.common.arguments.K2JsArgumentConstants
 import org.jetbrains.kotlin.cli.common.reportCompilationException
 import org.jetbrains.kotlin.cli.js.resolve
 import org.jetbrains.kotlin.cli.pipeline.PipelinePhase
 import org.jetbrains.kotlin.cli.pipeline.web.JsLoweredIrPipelineArtifact
 import org.jetbrains.kotlin.cli.pipeline.web.WebLoadedIrPipelineArtifact
-import org.jetbrains.kotlin.ir.backend.js.compileIr
+import org.jetbrains.kotlin.config.perfManager
+import org.jetbrains.kotlin.config.phaser.PhaserState
+import org.jetbrains.kotlin.ir.backend.js.*
+import org.jetbrains.kotlin.ir.backend.js.lower.collectNativeImplementations
+import org.jetbrains.kotlin.ir.backend.js.lower.generateJsTests
+import org.jetbrains.kotlin.ir.backend.js.lower.moveBodilessDeclarationsToSeparatePlace
+import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsIrLinker
+import org.jetbrains.kotlin.ir.util.ExternalDependenciesGenerator
 import org.jetbrains.kotlin.js.config.*
+import org.jetbrains.kotlin.util.PhaseType
+import org.jetbrains.kotlin.util.PotentiallyIncorrectPhaseTimeMeasurement
+import org.jetbrains.kotlin.util.tryMeasurePhaseTime
 
 object JsIrLoweringPipelinePhase : PipelinePhase<WebLoadedIrPipelineArtifact, JsLoweredIrPipelineArtifact>(
     name = "JsIrLoweringPipelinePhase",
 ) {
-    override fun executePhase(input: WebLoadedIrPipelineArtifact): JsLoweredIrPipelineArtifact? {
+    override fun executePhase(input: WebLoadedIrPipelineArtifact): JsLoweredIrPipelineArtifact? =
+        try {
+            lowerIr(input)
+        } catch (e: CompilationException) {
+            input.configuration.reportCompilationException(e)
+            null
+        }
+
+    private fun lowerIr(input: WebLoadedIrPipelineArtifact): JsLoweredIrPipelineArtifact {
         val configuration = input.configuration
         val module = input.moduleStructure
         val (moduleFragment, moduleDependencies, irBuiltIns, symbolTable, deserializer) =
             input.moduleInfo
-        try {
-            val loweredIr = compileIr(
-                moduleFragment = moduleFragment,
-                mainModule = module.mainModule,
-                mainCallArguments = emptyList<String>().takeUnless { configuration.callMainMode == K2JsArgumentConstants.NO_CALL },
-                configuration = configuration,
-                moduleDependencies = moduleDependencies,
-                irBuiltIns = irBuiltIns,
-                symbolTable = symbolTable,
-                irLinker = deserializer,
-                exportedDeclarations = configuration.additionalExportedDeclarationNames,
-                keep = configuration.keep.toSet(),
-                dceRuntimeDiagnostic = RuntimeDiagnostic.resolve(configuration.dceRuntimeDiagnostic, configuration),
-                safeExternalBoolean = configuration.safeExternalBoolean,
-                safeExternalBooleanDiagnostic = RuntimeDiagnostic.resolve(configuration.safeExternalBooleanDiagnostic, configuration),
-            )
-            return JsLoweredIrPipelineArtifact(loweredIr, configuration)
-        } catch (e: CompilationException) {
-            configuration.reportCompilationException(e)
-            return null
+        require(deserializer is JsIrLinker) {
+            "jsCompiler needs JsIrLinker, but got ${deserializer.javaClass.name}"
         }
+        val moduleDescriptor = moduleFragment.descriptor
+        val irFactory = symbolTable.irFactory
+        val shouldGeneratePolyfills = configuration.getBoolean(JSConfigurationKeys.GENERATE_POLYFILLS)
+        val performanceManager = configuration.perfManager
+        val context = JsIrBackendContext(
+            moduleDescriptor,
+            irBuiltIns = irBuiltIns,
+            symbolTable = symbolTable,
+            additionalExportedDeclarationNames = configuration.additionalExportedDeclarationNames,
+            keep = configuration.keep.toSet(),
+            configuration = configuration,
+            dceRuntimeDiagnostic = RuntimeDiagnostic.resolve(configuration.dceRuntimeDiagnostic, configuration),
+            safeExternalBoolean = configuration.safeExternalBoolean,
+            safeExternalBooleanDiagnostic = RuntimeDiagnostic.resolve(configuration.safeExternalBooleanDiagnostic, configuration),
+            incrementalCacheEnabled = false,
+            mainCallArguments = emptyList<String>().takeUnless { configuration.callMainMode == K2JsArgumentConstants.NO_CALL }
+        )
+        // Load declarations referenced during `context` initialization
+        val irProviders = listOf(element = deserializer)
+        ExternalDependenciesGenerator(symbolTable = symbolTable, irProviders = irProviders).generateUnboundSymbolsAsDependencies()
+        deserializer.postProcess(inOrAfterLinkageStep = true)
+        deserializer.checkNoUnboundSymbols(symbolTable = symbolTable, whenDetected = "at the end of IR linkage process")
+        deserializer.clear()
+        // Sort dependencies after IR linkage.
+        val sortedModuleDependencies = deserializer.moduleDependencyTracker.reverseTopoOrder(moduleDependencies = moduleDependencies)
+        val allModules = when (module.mainModule) {
+            is MainModule.SourceFiles -> error("Main module must be klib")
+            is MainModule.Klib -> sortedModuleDependencies.all
+        }
+        allModules.forEach { module ->
+            if (shouldGeneratePolyfills) {
+                collectNativeImplementations(context, module)
+            }
+
+            moveBodilessDeclarationsToSeparatePlace(context, module)
+        }
+        // TODO should be done incrementally
+        generateJsTests(context, allModules.last())
+        @OptIn(PotentiallyIncorrectPhaseTimeMeasurement::class)
+        performanceManager?.notifyCurrentPhaseFinishedIfNeeded() // It should be `notifyTranslationToIRFinished`, but this phase not always started or already finished
+        performanceManager.tryMeasurePhaseTime(PhaseType.IrLowering) {
+            (irFactory.stageController as? WholeWorldStageController)?.let {
+                lowerPreservingTags(allModules, context, it)
+            } ?: run {
+                val phaserState = PhaserState()
+                jsLowerings.forEachIndexed { _, lowering ->
+                    allModules.forEach { module ->
+                        lowering.invoke(context.phaseConfig, phaserState, context, module)
+                    }
+                }
+            }
+        }
+        val loweredIr = LoweredIr(
+            context,
+            mainModule = moduleFragment,
+            allModules = allModules,
+            moduleFragmentToUniqueName = moduleDependencies.fragmentNames
+        )
+        return JsLoweredIrPipelineArtifact(loweredIr, configuration)
     }
 }
--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/jsCompiler.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/jsCompiler.kt
@@ -5,28 +5,9 @@
 
 package org.jetbrains.kotlin.ir.backend.js
 
-import org.jetbrains.kotlin.backend.common.IrModuleDependencies
-import org.jetbrains.kotlin.backend.common.linkage.issues.checkNoUnboundSymbols
-import org.jetbrains.kotlin.backend.common.serialization.KotlinIrLinker
-import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.perfManager
-import org.jetbrains.kotlin.config.phaser.PhaserState
-import org.jetbrains.kotlin.ir.IrBuiltIns
-import org.jetbrains.kotlin.ir.backend.js.lower.collectNativeImplementations
-import org.jetbrains.kotlin.ir.backend.js.lower.generateJsTests
-import org.jetbrains.kotlin.ir.backend.js.lower.moveBodilessDeclarationsToSeparatePlace
-import org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir.JsIrLinker
 import org.jetbrains.kotlin.ir.backend.js.transformers.irToJs.CompilationOutputs
 import org.jetbrains.kotlin.ir.backend.js.transformers.irToJs.TranslationMode
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.util.ExternalDependenciesGenerator
-import org.jetbrains.kotlin.ir.util.SymbolTable
-import org.jetbrains.kotlin.js.config.JSConfigurationKeys
-import org.jetbrains.kotlin.js.config.RuntimeDiagnostic
-import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.util.PhaseType
-import org.jetbrains.kotlin.util.PotentiallyIncorrectPhaseTimeMeasurement
-import org.jetbrains.kotlin.util.tryMeasurePhaseTime
 
 class CompilerResult(
     val outputs: Map<TranslationMode, CompilationOutputs>,
@@ -38,85 +19,3 @@ class LoweredIr(
     val allModules: List<IrModuleFragment>,
     val moduleFragmentToUniqueName: Map<IrModuleFragment, String>,
 )
-
-fun compileIr(
-    moduleFragment: IrModuleFragment,
-    mainModule: MainModule,
-    mainCallArguments: List<String>?,
-    configuration: CompilerConfiguration,
-    moduleDependencies: IrModuleDependencies,
-    irBuiltIns: IrBuiltIns,
-    symbolTable: SymbolTable,
-    irLinker: KotlinIrLinker,
-    exportedDeclarations: Set<FqName>,
-    keep: Set<String>,
-    dceRuntimeDiagnostic: RuntimeDiagnostic?,
-    safeExternalBoolean: Boolean,
-    safeExternalBooleanDiagnostic: RuntimeDiagnostic?,
-): LoweredIr {
-    require(irLinker is JsIrLinker) {
-        "jsCompiler needs JsIrLinker, but got ${irLinker.javaClass.name}"
-    }
-    val moduleDescriptor = moduleFragment.descriptor
-    val irFactory = symbolTable.irFactory
-    val shouldGeneratePolyfills = configuration.getBoolean(JSConfigurationKeys.GENERATE_POLYFILLS)
-    val performanceManager = configuration.perfManager
-
-    val context = JsIrBackendContext(
-        moduleDescriptor,
-        irBuiltIns,
-        symbolTable,
-        exportedDeclarations,
-        keep,
-        configuration,
-        dceRuntimeDiagnostic = dceRuntimeDiagnostic,
-        safeExternalBoolean = safeExternalBoolean,
-        safeExternalBooleanDiagnostic = safeExternalBooleanDiagnostic,
-        incrementalCacheEnabled = false,
-        mainCallArguments = mainCallArguments
-    )
-
-    // Load declarations referenced during `context` initialization
-    val irProviders = listOf(irLinker)
-    ExternalDependenciesGenerator(symbolTable, irProviders).generateUnboundSymbolsAsDependencies()
-
-    irLinker.postProcess(inOrAfterLinkageStep = true)
-    irLinker.checkNoUnboundSymbols(symbolTable, "at the end of IR linkage process")
-    irLinker.clear()
-
-    // Sort dependencies after IR linkage.
-    val sortedModuleDependencies = irLinker.moduleDependencyTracker.reverseTopoOrder(moduleDependencies)
-
-    val allModules = when (mainModule) {
-        is MainModule.SourceFiles -> error("Main module must be klib")
-        is MainModule.Klib -> sortedModuleDependencies.all
-    }
-
-    allModules.forEach { module ->
-        if (shouldGeneratePolyfills) {
-            collectNativeImplementations(context, module)
-        }
-
-        moveBodilessDeclarationsToSeparatePlace(context, module)
-    }
-
-    // TODO should be done incrementally
-    generateJsTests(context, allModules.last())
-    @OptIn(PotentiallyIncorrectPhaseTimeMeasurement::class)
-    performanceManager?.notifyCurrentPhaseFinishedIfNeeded() // It should be `notifyTranslationToIRFinished`, but this phase not always started or already finished
-
-    performanceManager.tryMeasurePhaseTime(PhaseType.IrLowering) {
-        (irFactory.stageController as? WholeWorldStageController)?.let {
-            lowerPreservingTags(allModules, context, it)
-        } ?: run {
-            val phaserState = PhaserState()
-            jsLowerings.forEachIndexed { _, lowering ->
-                allModules.forEach { module ->
-                    lowering.invoke(context.phaseConfig, phaserState, context, module)
-                }
-            }
-        }
-    }
-
-    return LoweredIr(context, moduleFragment, allModules, moduleDependencies.fragmentNames)
-}
