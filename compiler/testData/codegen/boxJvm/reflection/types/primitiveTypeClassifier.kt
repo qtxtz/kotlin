qtxtz/kotlin

commit 80bbc4b61da455daa8a65b30afb127da5870b2a9

Reflection: support descriptor-less Kotlin constructors
 #KT-80710

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ConvertFromMetadata.kt
@@ -316,3 +316,11 @@ internal fun createUnboundFunction(function: KmFunction, container: KDeclaration
         ?: throw KotlinReflectionInternalError("No signature for function: ${function.name}")
     return KotlinKNamedFunction(container, signature, CallableReference.NO_RECEIVER, function)
 }
+
+internal fun createUnboundConstructor(constructor: KmConstructor, container: KDeclarationContainerImpl): KotlinKFunction {
+    val signature = constructor.signature?.toString()
+        ?: throw KotlinReflectionInternalError(
+            "No signature for constructor (${constructor.valueParameters.size} parameters, declared in $container)"
+        )
+    return KotlinKConstructor(container, signature, CallableReference.NO_RECEIVER, constructor)
+}

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLBlackBoxTestGenerated.java
@@ -54511,6 +54511,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       }
 
       @Test
+      @TestMetadata("constructorParameters.kt")
+      public void testConstructorParameters() {
+        runTest("compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt");
+      }
+
+      @Test
       @TestMetadata("findAnnotation.kt")
       public void testFindAnnotation() {
         runTest("compiler/testData/codegen/box/reflection/annotations/findAnnotation.kt");
@@ -55114,6 +55120,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
         }
 
         @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt");
+        }
+
+        @Test
         @TestMetadata("secondaryConstructorVisibilies.kt")
         public void testSecondaryConstructorVisibilies() {
           runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/secondaryConstructorVisibilies.kt");
@@ -55661,6 +55673,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
           KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/reflection/callBy/inlineClasses"), Pattern.compile("^(.+)\\.kt$"), null, true);
         }
 
+        @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt");
+        }
+
         @Nested
         @TestMetadata("compiler/testData/codegen/box/reflection/callBy/inlineClasses/nonNullObject")
         @TestDataPath("$PROJECT_ROOT")
@@ -56179,6 +56197,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       }
 
       @Test
+      @TestMetadata("innerClassOfGenericClass.kt")
+      public void testInnerClassOfGenericClass() {
+        runTest("compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt");
+      }
+
+      @Test
       @TestMetadata("primaryConstructor.kt")
       public void testPrimaryConstructor() {
         runTest("compiler/testData/codegen/box/reflection/constructors/primaryConstructor.kt");
@@ -56592,12 +56616,6 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
       @TestMetadata("constructor.kt")
       public void testConstructor() {
         runTest("compiler/testData/codegen/box/reflection/mapping/constructor.kt");
-      }
-
-      @Test
-      @TestMetadata("constructorWithInlineClassParameters.kt")
-      public void testConstructorWithInlineClassParameters() {
-        runTest("compiler/testData/codegen/box/reflection/mapping/constructorWithInlineClassParameters.kt");
       }
 
       @Test

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
@@ -54511,6 +54511,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       }
 
       @Test
+      @TestMetadata("constructorParameters.kt")
+      public void testConstructorParameters() {
+        runTest("compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt");
+      }
+
+      @Test
       @TestMetadata("findAnnotation.kt")
       public void testFindAnnotation() {
         runTest("compiler/testData/codegen/box/reflection/annotations/findAnnotation.kt");
@@ -55114,6 +55120,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
         }
 
         @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt");
+        }
+
+        @Test
         @TestMetadata("secondaryConstructorVisibilies.kt")
         public void testSecondaryConstructorVisibilies() {
           runTest("compiler/testData/codegen/box/reflection/call/inlineClasses/secondaryConstructorVisibilies.kt");
@@ -55661,6 +55673,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
           KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/reflection/callBy/inlineClasses"), Pattern.compile("^(.+)\\.kt$"), null, true);
         }
 
+        @Test
+        @TestMetadata("result.kt")
+        public void testResult() {
+          runTest("compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt");
+        }
+
         @Nested
         @TestMetadata("compiler/testData/codegen/box/reflection/callBy/inlineClasses/nonNullObject")
         @TestDataPath("$PROJECT_ROOT")
@@ -56179,6 +56197,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       }
 
       @Test
+      @TestMetadata("innerClassOfGenericClass.kt")
+      public void testInnerClassOfGenericClass() {
+        runTest("compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt");
+      }
+
+      @Test
       @TestMetadata("primaryConstructor.kt")
       public void testPrimaryConstructor() {
         runTest("compiler/testData/codegen/box/reflection/constructors/primaryConstructor.kt");
@@ -56592,12 +56616,6 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
       @TestMetadata("constructor.kt")
       public void testConstructor() {
         runTest("compiler/testData/codegen/box/reflection/mapping/constructor.kt");
-      }
-
-      @Test
-      @TestMetadata("constructorWithInlineClassParameters.kt")
-      public void testConstructorWithInlineClassParameters() {
-        runTest("compiler/testData/codegen/box/reflection/mapping/constructorWithInlineClassParameters.kt");
       }
 
       @Test

compiler/testData/codegen/box/reflection/annotations/constructorParameters.kt
@@ -1,0 +1,60 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FILE: test/J.java
+package test;
+
+public class J {
+    public J(@Anno("J") String s) {}
+
+    public class Inner {
+        public Inner(@Anno("Inner") int x) {}
+    }
+}
+
+// FILE: test/JEnum.java
+package test;
+
+public enum JEnum {
+    ;
+    JEnum(@Anno("JEnum") double d) {}
+}
+
+// FILE: test/box.kt
+package test
+
+import kotlin.reflect.KClass
+import kotlin.test.assertEquals
+
+annotation class Anno(val value: String)
+
+class K(@Anno("K") f: Float) {
+    inner class Inner(@Anno("Inner") j: Long)
+}
+
+enum class KEnum(@Anno("KEnum") z: Boolean)
+
+sealed class Sealed(@Anno("Sealed") s: Short)
+
+class Default(@Anno("Default") n: Number? = null)
+
+sealed class SealedWithDefault(@Anno("SealedWithDefault") a: Any? = null)
+
+private val KClass<*>.ctorParamAnnotations: String
+    get() = constructors.single().parameters.joinToString(", ") { p ->
+        p.annotations.map { (it as Anno).value }.toString()
+    }
+
+fun box(): String {
+    assertEquals("[J]", J::class.ctorParamAnnotations)
+    assertEquals("[], [Inner]", J.Inner::class.ctorParamAnnotations)
+    assertEquals("[JEnum]", JEnum::class.ctorParamAnnotations)
+
+    assertEquals("[K]", K::class.ctorParamAnnotations)
+    assertEquals("[], [Inner]", K.Inner::class.ctorParamAnnotations)
+    assertEquals("[KEnum]", KEnum::class.ctorParamAnnotations)
+    assertEquals("[Sealed]", Sealed::class.ctorParamAnnotations)
+    assertEquals("[Default]", Default::class.ctorParamAnnotations)
+    assertEquals("[SealedWithDefault]", SealedWithDefault::class.ctorParamAnnotations)
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/call/inlineClasses/result.kt
@@ -1,0 +1,18 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+
+import kotlin.reflect.jvm.isAccessible
+
+class C(val x: Result<Int>)
+
+fun box(): String {
+    val resultCtor = Result::class.constructors.single()
+    val r = resultCtor.apply { isAccessible = true }.call(42)
+    if (r != Result.success(42)) return "Fail 1: $r"
+
+    val ctorWithResult = ::C
+    val s = ctorWithResult.call(r).x
+    if (s != Result.success(42)) return "Fail 2: $s"
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/callBy/inlineClasses/result.kt
@@ -1,0 +1,18 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+
+import kotlin.reflect.jvm.isAccessible
+
+class C(val x: Result<Int>)
+
+fun box(): String {
+    val resultCtor = Result::class.constructors.single()
+    val r = resultCtor.apply { isAccessible = true }.callBy(mapOf(resultCtor.parameters.single() to 42))
+    if (r != Result.success(42)) return "Fail 1: $r"
+
+    val ctorWithResult = ::C
+    val s = ctorWithResult.callBy(mapOf(ctorWithResult.parameters.single() to r)).x
+    if (s != Result.success(42)) return "Fail 2: $s"
+
+    return "OK"
+}

compiler/testData/codegen/box/reflection/constructors/classesWithoutConstructors.kt
@@ -2,7 +2,7 @@
 // WITH_REFLECT
 
 import kotlin.coroutines.SuspendFunction0
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 interface Interface
 object Obj
@@ -12,12 +12,12 @@ class C {
 }
 
 fun box(): String {
-    assertTrue(Interface::class.constructors.isEmpty())
-    assertTrue(Obj::class.constructors.isEmpty())
-    assertTrue(C.Companion::class.constructors.isEmpty())
-    assertTrue(object {}::class.constructors.isEmpty())
-    assertTrue(Function0::class.constructors.isEmpty())
-    assertTrue(SuspendFunction0::class.constructors.isEmpty())
+    assertEquals(emptyList(), Interface::class.constructors)
+    assertEquals(emptyList(), Obj::class.constructors)
+    assertEquals(emptyList(), C.Companion::class.constructors)
+    assertEquals(emptyList(), object {}::class.constructors)
+    assertEquals(emptyList(), Function0::class.constructors)
+    assertEquals(emptyList(), SuspendFunction0::class.constructors)
 
     return "OK"
 }

compiler/testData/codegen/box/reflection/constructors/innerClassOfGenericClass.kt
@@ -1,0 +1,20 @@
+// TARGET_BACKEND: JVM
+// IGNORE_BACKEND_K1: JVM_IR
+// WITH_REFLECT
+package test
+
+import kotlin.test.assertEquals
+
+class C<A, B> {
+    class Nested
+    inner class Inner(a: A)
+}
+
+fun box(): String {
+    assertEquals("fun `<init>`(): test.C.Nested", C<*, *>::Nested.toString())
+    assertEquals("fun test.C<A, B>.`<init>`(A): test.C<A, B>.Inner", C<*, *>::Inner.toString())
+
+    assertEquals(C::class.typeParameters, C<*, *>::Inner.returnType.arguments.map { it.type?.classifier })
+
+    return "OK"
+}

commit 63bf058ccefe74c71bd96e9beb458a6368274fe5

Reflection: correctly load underlying Java classes of primitive types
This change repeats the behavior that was present in the old K1-based
reflection at `DescriptorKType.convert` (and was apparently not
well-tested), which made the converted `KClass` objects for primitive
types have correct underlying Java `Class` object.

There are three rules which define the behavior:
1) Type `Int?` -> classifier's Java class is wrapper class
2) Type `Int` in the type argument position (`C<Int>`) or in the type
   parameter bound position (`T : Int`) -> wrapper class
3) Type `Int` in all other cases -> primitive class

 #RCA: caused by 80bbc4b61da4 (part of KT-80710) where a part of
affected behavior was not fully tested, now it is.

 #KT-85550 Fixed

core/descriptors.runtime/src/org/jetbrains/kotlin/descriptors/runtime/structure/reflectClassUtil.kt
@@ -19,9 +19,9 @@ package org.jetbrains.kotlin.descriptors.runtime.structure
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import java.lang.reflect.Array
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
+import kotlin.reflect.KClass
 
 val Class<*>.safeClassLoader: ClassLoader
     get() = classLoader ?: ClassLoader.getSystemClassLoader()
@@ -29,10 +29,10 @@ val Class<*>.safeClassLoader: ClassLoader
 fun Class<*>.isEnumClassOrSpecializedEnumEntryClass(): Boolean =
     Enum::class.java.isAssignableFrom(this)
 
-private val PRIMITIVE_CLASSES =
+private val PRIMITIVE_CLASSES: List<KClass<*>> =
     listOf(Boolean::class, Byte::class, Char::class, Double::class, Float::class, Int::class, Long::class, Short::class)
-private val WRAPPER_TO_PRIMITIVE = PRIMITIVE_CLASSES.map { it.javaObjectType to it.javaPrimitiveType }.toMap()
-private val PRIMITIVE_TO_WRAPPER = PRIMITIVE_CLASSES.map { it.javaPrimitiveType to it.javaObjectType }.toMap()
+private val WRAPPER_TO_PRIMITIVE: Map<Class<*>, Class<*>?> = PRIMITIVE_CLASSES.associate { it.javaObjectType to it.javaPrimitiveType }
+private val PRIMITIVE_TO_WRAPPER: Map<Class<*>?, Class<*>> = PRIMITIVE_CLASSES.associate { it.javaPrimitiveType to it.javaObjectType }
 
 val Class<*>.primitiveByWrapper: Class<*>?
     get() = WRAPPER_TO_PRIMITIVE[this]

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ConvertFromMetadata.kt
@@ -7,6 +7,7 @@ package kotlin.reflect.jvm.internal
 
 import org.jetbrains.kotlin.builtins.jvm.JavaToKotlinClassMap
 import org.jetbrains.kotlin.descriptors.runtime.structure.parameterizedTypeArguments
+import org.jetbrains.kotlin.descriptors.runtime.structure.primitiveByWrapper
 import org.jetbrains.kotlin.load.java.JvmAbi
 import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmProtoBufUtil
 import org.jetbrains.kotlin.name.ClassId
@@ -20,18 +21,10 @@ import kotlin.LazyThreadSafetyMode.PUBLICATION
 import kotlin.coroutines.Continuation
 import kotlin.jvm.internal.CallableReference
 import kotlin.metadata.*
-import kotlin.metadata.jvm.annotations
-import kotlin.metadata.jvm.fieldSignature
-import kotlin.metadata.jvm.getterSignature
-import kotlin.metadata.jvm.isRaw
-import kotlin.metadata.jvm.signature
+import kotlin.metadata.jvm.*
 import kotlin.reflect.*
 import kotlin.reflect.jvm.internal.calls.createAnnotationInstance
-import kotlin.reflect.jvm.internal.types.AbstractKType
-import kotlin.reflect.jvm.internal.types.FlexibleKType
-import kotlin.reflect.jvm.internal.types.MutableCollectionKClass
-import kotlin.reflect.jvm.internal.types.SimpleKType
-import kotlin.reflect.jvm.internal.types.getMutableCollectionKClass
+import kotlin.reflect.jvm.internal.types.*
 import kotlin.reflect.jvm.jvmErasure
 
 internal fun ClassName.toClassId(): ClassId {
@@ -49,8 +42,8 @@ internal fun ClassName.toNonLocalSimpleName(): String {
     return substringAfterLast('/').substringAfterLast('.')
 }
 
-internal fun ClassLoader.loadKClass(name: ClassName): KClass<*>? =
-    loadClass(name.toClassId())?.kotlin
+internal fun ClassLoader.loadKClass(name: ClassName, forceWrapperClass: Boolean = false): KClass<*>? =
+    loadClass(name.toClassId())?.let { if (forceWrapperClass) it else it.primitiveByWrapper ?: it }?.kotlin
 
 /**
  * Provides the access to the type parameters of a Kotlin declaration, and allows to obtain a type parameter given its id.
@@ -89,8 +82,10 @@ internal class TypeParameterTable private constructor(
             val map = kmTypeParameters.withIndex().associate { (index, km) -> km.id to kTypeParameters[index] }
             return TypeParameterTable(kTypeParameters, map, parent).also { table ->
                 for ((i, typeParameter) in kTypeParameters.withIndex()) {
-                    typeParameter.upperBounds = kmTypeParameters[i].upperBounds.map { it.toKType(classLoader, table) }
-                        .ifEmpty { listOf(StandardKTypes.NULLABLE_ANY) }
+                    typeParameter.upperBounds = kmTypeParameters[i].upperBounds.map {
+                        // Upper bounds of type parameters should always have underlying wrapper (not primitive) classes.
+                        it.toKType(classLoader, table, forceWrapperClass = true)
+                    }.ifEmpty { listOf(StandardKTypes.NULLABLE_ANY) }
                 }
             }
         }
@@ -100,19 +95,32 @@ internal class TypeParameterTable private constructor(
 internal fun KmType.toKType(
     classLoader: ClassLoader,
     typeParameterTable: TypeParameterTable,
+    forceWrapperClass: Boolean = false,
     computeJavaType: (() -> Type)? = null,
-): KType {
+): AbstractKType {
     lateinit var result: SimpleKType
     val arguments = generateSequence(this) { it.outerType }
         .flatMap { it.arguments }
         .mapIndexed { i, typeArgument ->
-            typeArgument.toKTypeProjection(
-                classLoader, typeParameterTable,
-                if (computeJavaType == null) null else convertTypeArgumentToJavaType({ result }, i)
-            )
+            if (typeArgument == KmTypeProjection.STAR)
+                KTypeProjection.STAR
+            else {
+                // Non-null type `kotlin/Int` should have a `KClass` with a primitive class (`int.class` or `Integer.TYPE`), except for the
+                // case when it's a generic type argument, where it should be a `KClass` over the wrapper class.
+                val argumentType = typeArgument.type?.toKType(
+                    classLoader,
+                    typeParameterTable,
+                    forceWrapperClass = true,
+                    if (computeJavaType == null) null else convertTypeArgumentToJavaType({ result }, i),
+                )
+                KTypeProjection(typeArgument.variance?.toKVariance(), argumentType)
+            }
         }
         .toList()
-    val kClassifier = classifier.toClassifier(classLoader, typeParameterTable, arguments)
+
+    // Nullable type `kotlin/Int?` should always have a `KClass` with underlying wrapper class (`Integer.class` in Java syntax).
+    val kClassifier = classifier.toClassifier(classLoader, typeParameterTable, arguments, isNullable || forceWrapperClass)
+
     result = SimpleKType(
         kClassifier,
         arguments,
@@ -132,7 +140,7 @@ internal fun KmType.toKType(
     }
     flexibleTypeUpperBound?.let {
         if (it.typeFlexibilityId == JvmProtoBufUtil.PLATFORM_TYPE_ID) {
-            return FlexibleKType.create(result, it.type.toKType(classLoader, typeParameterTable) as SimpleKType, isRaw, computeJavaType)
+            return FlexibleKType.create(result, it.type.toKType(classLoader, typeParameterTable), isRaw, computeJavaType)
         }
     }
     return result
@@ -181,13 +189,17 @@ internal fun convertTypeArgumentToJavaType(computeType: () -> AbstractKType, ind
 }
 
 private fun KmClassifier.toClassifier(
-    classLoader: ClassLoader, typeParameterTable: TypeParameterTable, typeArguments: List<KTypeProjection>,
+    classLoader: ClassLoader,
+    typeParameterTable: TypeParameterTable,
+    typeArguments: List<KTypeProjection>,
+    forceWrapperClass: Boolean,
 ): KClassifier = when (this) {
     is KmClassifier.Class ->
         if (name == "kotlin/Array")
             (typeArguments.single().type ?: StandardKTypes.ANY).jvmErasure.java.createArrayType().kotlin
         else
-            classLoader.loadKClass(name) ?: throw KotlinReflectionInternalError("Class not found: $name")
+            classLoader.loadKClass(name, forceWrapperClass)
+                ?: throw KotlinReflectionInternalError("Class not found: $name")
     is KmClassifier.TypeAlias ->
         KTypeAliasImpl(name.toClassId().asSingleFqName())
     is KmClassifier.TypeParameter ->
@@ -200,16 +212,6 @@ private fun KmClassifier.toClassifier(
 internal class ErrorTypeParameter(private val id: Int) : KClassifier {
     override fun toString(): String = "[Error type parameter $id]"
 }
-
-private fun KmTypeProjection.toKTypeProjection(
-    classLoader: ClassLoader,
-    typeParameterTable: TypeParameterTable,
-    computeJavaType: (() -> Type)?,
-): KTypeProjection =
-    if (this == KmTypeProjection.STAR)
-        KTypeProjection.STAR
-    else
-        KTypeProjection(variance?.toKVariance(), type?.toKType(classLoader, typeParameterTable, computeJavaType))
 
 internal fun KmVariance.toKVariance(): KVariance = when (this) {
     KmVariance.IN -> KVariance.IN

core/reflection.jvm/src/kotlin/reflect/jvm/internal/KotlinKProperty.kt
@@ -37,9 +37,10 @@ internal abstract class KotlinKProperty<out V>(
     }
 
     override val returnType: KType by lazy(PUBLICATION) {
-        kmProperty.returnType.toKType(container.jClass.classLoader, typeParameterTable.value, if (isLocalDelegated) null else fun(): Type {
-            return caller.returnType
-        })
+        kmProperty.returnType.toKType(
+            container.jClass.classLoader, typeParameterTable.value,
+            computeJavaType = if (isLocalDelegated) null else fun(): Type = caller.returnType,
+        )
     }
 
     val typeParameterTable: Lazy<TypeParameterTable> = lazy(PUBLICATION) {

compiler/testData/codegen/boxJvm/reflection/types/primitiveTypeClassifier.kt
@@ -1,0 +1,77 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FILE: J.java
+public class J {
+    public J(char primitive, Character wrapper) {}
+}
+
+// FILE: box.kt
+import kotlin.reflect.*
+import kotlin.reflect.full.findAnnotation
+import kotlin.test.assertEquals
+
+class A(
+    val primitive: Boolean,
+    val wrapper: Boolean?,
+)
+
+fun primitive(): Byte = 0.toByte()
+fun wrapper(): Byte? = null
+
+@Target(AnnotationTarget.TYPE)
+annotation class Anno(val value: IntArray)
+fun primitiveInAnnoArrayArg(): @Anno([1, 2, 3]) Unit {}
+
+fun primitiveArray(): IntArray = intArrayOf(1)
+
+class B<T : Float>
+
+fun array(): Array<Long> = arrayOf(1L)
+fun list(): List<Double> = listOf(1.0)
+
+private fun KType.javaClass(): Class<*> = (classifier as KClass<*>).java
+
+fun box(): String {
+    assertEquals(
+        listOfNotNull(Char::class.javaPrimitiveType, Char::class.javaObjectType),
+        J::class.constructors.single().parameters.map { it.type.javaClass() },
+    )
+
+    assertEquals(
+        listOfNotNull(Boolean::class.javaPrimitiveType, Boolean::class.javaObjectType),
+        A::class.constructors.single().parameters.map { it.type.javaClass() },
+    )
+
+    assertEquals(Byte::class.javaPrimitiveType!!, ::primitive.returnType.javaClass())
+    assertEquals(Byte::class.javaObjectType, ::wrapper.returnType.javaClass())
+
+    assertEquals(Short::class.javaPrimitiveType!!, typeOf<Short>().javaClass())
+    assertEquals(Short::class.javaObjectType, typeOf<Short?>().javaClass())
+
+    assertEquals(
+        Int::class.javaPrimitiveType!!,
+        ::primitiveInAnnoArrayArg.returnType.findAnnotation<Anno>()!!.value::class.java.componentType,
+    )
+
+    assertEquals(Int::class.javaPrimitiveType!!, ::primitiveArray.returnType.javaClass().componentType)
+
+    if (Class.forName("kotlin.reflect.jvm.internal.SystemPropertiesKt").getMethod("getUseK1Implementation").invoke(null) == true) {
+        assertEquals(Float::class.javaPrimitiveType!!, B::class.typeParameters.single().upperBounds.single().javaClass())
+    } else {
+        assertEquals(Float::class.javaObjectType, B::class.typeParameters.single().upperBounds.single().javaClass())
+    }
+
+    if (Class.forName("kotlin.reflect.jvm.internal.SystemPropertiesKt").getMethod("getUseK1Implementation").invoke(null) == true) {
+        assertEquals(Long::class.javaPrimitiveType!!, ::array.returnType.arguments.single().type!!.javaClass())
+    } else {
+        assertEquals(Long::class.javaObjectType, ::array.returnType.arguments.single().type!!.javaClass())
+    }
+
+    if (Class.forName("kotlin.reflect.jvm.internal.SystemPropertiesKt").getMethod("getUseK1Implementation").invoke(null) == true) {
+        assertEquals(Double::class.javaPrimitiveType!!, ::list.returnType.arguments.single().type!!.javaClass())
+    } else {
+        assertEquals(Double::class.javaObjectType, ::list.returnType.arguments.single().type!!.javaClass())
+    }
+
+    return "OK"
+}