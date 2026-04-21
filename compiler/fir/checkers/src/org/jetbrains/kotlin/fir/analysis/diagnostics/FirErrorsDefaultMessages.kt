ba0f5cc08d56b79c5bc4966824a18a1dcd2e65c2
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-04-21 12:56:20 +0200

    [FIR] Update message for NULLABLE_ON_DEFINITELY_NOT_NULLABLE
    #KT-85841 Fixed

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/diagnostics/FirErrorsDefaultMessages.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/diagnostics/FirErrorsDefaultMessages.kt
@@ -2321,7 +2321,7 @@ object FirErrorsDefaultMessages : BaseDiagnosticRendererFactory() {
         )
         map.put(
             NULLABLE_ON_DEFINITELY_NOT_NULLABLE,
-            "'!!' type cannot be marked as nullable.",
+            "Definitely non-nullable type cannot be marked as nullable.",
         )
         map.put(
             REDUNDANT_NULLABLE,
